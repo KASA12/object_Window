09b3cb1785c206269a20fad22ccd94223a326dff
KASANAMI Masaaki <saw.toothed.coastline.12@gmail.com>
2019-03-09 19:36:46 +0900

    文字数削減。

--- docs/javascripts/script.js
+++ docs/javascripts/script.js
@@ -1,17 +1,15 @@
 s = 600
-d = 0.3
 setup = _ => {
-  createCanvas(s, s);
-  pixelDensity(d)
+  createCanvas(s, s)
+  pixelDensity(0.3)
 }
 draw = _ => {
   textSize(70)
   text(this, 0, s/2)
   loadPixels()
-  p = pixels;
   for (i = 0; i < 90000; i += map(mouseX, 0, s, 2, 32)) {
     for (j = 0; j < 3; j++){
-      p[i * 4 + j] ^= 255
+      pixels[i * 4 + j] ^= 255
     }
   }
   updatePixels()
